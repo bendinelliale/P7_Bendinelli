df7d108a5afd723c77043ab3f85b3a0f421536f8
bendinelliale <95869447+bendinelliale@users.noreply.github.com>
2022-07-19 17:21:09 +0100

    90%

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-
-import Navbar from './navbar';
-import Home from './Home';
+import './css/styles.css';
+import {BrowserRouter as Router, Routes, Route, Link,} from 'react-router-dom'
+import React,{ useEffect, useState } from 'react';
+import Login from './components/login';
+import Register from './components/register';
+import HomePage from './components/homepage';
+import Error404 from './components/error404';
+import NewPost from './components/newpost';
+import Archive from './components/archive';
+import { Navigate} from 'react-router-dom';
 
 function App() {
-  const title = 'Welcome to Grupomania'
-  return (
-    <div className="App">
-      <Navbar />
-    <div className="content">
-      <Home />
-    </div>
-    </div>
-  );
-}
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-export default App;
+  useEffect( () =>{
+
+  },[])
+
+  return (
+    <Router>
+      <div className='wrapper'>
+      <div className='container-fluid'>
+			</div>
+        <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/newpost' element={<NewPost />} />
+        <Route path='/archive' element={<Archive />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+      </div>
+    </Router>
+  )
+}
+export default App
+
+
